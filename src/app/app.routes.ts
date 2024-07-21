555e6299f5ba389d17a09c07490336d939093c9d
fuceda <fransanchezuceda@gmail.com>
2024-07-21 20:25:02 +0200

    Dev 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CategoriasListComponent } from './components/categorias-list/categorias-list.component';
+import { ProductosListComponent } from './components/productos-list/productos-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'lista-categorias', component:CategoriasListComponent
+    },
+    {
+        path:'lista-productos', component: ProductosListComponent
+    }
+
+];
